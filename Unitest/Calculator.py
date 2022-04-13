820f5ab263984012f6e96a03c3c51e6c13972b86
AnastasiyaMava <matyshava@gmail.com>
2022-04-13 15:25:30 +0300

    Calculator

---
+++ Unitest/Calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def sum(self, x1, x2):
+        return x1 + x2
+
+    def diduction(self, x1, x2):
+        return x1 - x2
+
+    def multiplication(self, x1, x2):
+        return x1 * x2
+
+    def division(self, x1, x2):
+        if x2 != 0:
+            return x1 / x2
