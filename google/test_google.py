37723bae89930c1308a28b6dc308a2fd3a7c476a
AnastasiyaMava <matyshava@gmail.com>
2022-05-26 19:02:03 +0300

    test

--- google/test_google.py
+++ google/test_google.py
@@ -5,12 +5,13 @@ from selenium import webdriver
 @pytest.fixture(scope='class')
 def web_driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('headless')
+    chrome_options.add_argument('--disable-infobars')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--remote-debugging-port=9222')
     driver = webdriver.Chrome(chrome_options=chrome_options,
-                              executable_path='./chromedriver')
+                              executable_path='/snap/bin/chromium.chromedriver')
     driver.maximize_window()
     driver.get("https://www.google.com/")
     driver.implicitly_wait(10)
