48015d28fcc0787c70ff56a922d9372ec3d05265
AnastasiyaMava <matyshava@gmail.com>
2022-05-26 14:53:19 +0300

    test

---
+++ google/test_google.py
@@ -0,0 +1,24 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope='class')
+def web_driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(chrome_options=chrome_options,
+                              executable_path='/home/anastasiya/tms_05_HW/google/chromedriver')
+    driver.maximize_window()
+    driver.get("https://www.google.com/")
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+class TestGoogle:
+
+    def test_url(self, web_driver):
+        current_url = web_driver.current_url
+        assert current_url == "https://www.google.com/"
---
+++ google/test_page.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import unittest
+
+
+class TestMyStore(unittest.TestCase):
+    browser = None
+    url = "http://automationpractice.com/index.php?controller=authentication&back=my-account"
+    mail = 'ndobatovkina@yandex.ru'
+    password = '123456'
+
+    @classmethod
+    def setUpClass(cls):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        cls.browser = webdriver.Chrome(chrome_options=chrome_options,
+                                       executable_path='/home/anastasiya/tms_05_HW/google/chromedriver')
+        cls.browser.maximize_window()
+        cls.browser.get(cls.url)
+        cls.browser.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+
+    def test_authorization(self):
+        email = self.browser.find_element(by=By.ID, value="email")
+        email.click()
+        email.send_keys(self.mail)
+
+        password = self.browser.find_element(by=By.ID, value="passwd")
+        password.click()
+        password.send_keys(self.password)
+
+        submit_button = self.browser.find_element(by=By.ID,
+                                                  value="SubmitLogin")
+        submit_button.submit()
+
+        my_account = self.browser.current_url
+        self.assertEqual(my_account,
+                         "http://automationpractice.com/index.php?controller=authentication")
