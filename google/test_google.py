65bb5865b7b3ac950625740207aeb4d96583dfe8
AnastasiyaMava <matyshava@gmail.com>
2022-05-26 18:53:06 +0300

    test

--- google/test_google.py
+++ google/test_google.py
@@ -8,6 +8,7 @@ def web_driver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(chrome_options=chrome_options,
                               executable_path='./chromedriver')
     driver.maximize_window()
